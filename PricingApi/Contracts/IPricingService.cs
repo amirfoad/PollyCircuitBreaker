b08e45e69972e79f7c9c74d5980b6e7ea0410d3d
Amirfoad <55051145+amirfoad@users.noreply.github.com>
2023-09-04 15:21:04 +0330

    Delete PricingApi directory

--- PricingApi/Contracts/IPricingService.cs
+++
@@ -1,8 +0,0 @@
-﻿using PricingApi.Models;
-
-namespace PricingApi.Contracts;
-
-public interface IPricingService
-{
-    Task<PricingDetails> GetPricingForProductAsync(Guid productId,string currencyCode);
-}
